cc88b93426801df41234d76edc6f4c0037e766ca
MaxAnii <ansarbaba2000@gmail.com>
2024-10-22 11:53:21 +0530

    chore: adds try except block.

--- summerize_model/views.py
+++ summerize_model/views.py
@@ -2,18 +2,19 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-# from .services import TransformerService
 from .services import TransformerService
-
-
-
-
 
 transformer_service = TransformerService()
 
+
 @api_view(['POST'])
 def summerize_text(request):
-    text = request.data.get('text')
-    summary = transformer_service.summarize(text)
-    return Response({'message':summary})
-# Create your views here.
+    try:
+        text = request.data.get('text')
+        if not text:
+            return Response({'message':"No text found"}, status=status.HTTP_204_NO_CONTENT)
+        summary = transformer_service.summarize(text)
+        return Response({'message':summary}, status=status.HTTP_200_OK)
+    except Exception as error:
+        return Response({'message':str(error)}, status=status.HTTP_400_BAD_REQUEST)
+
