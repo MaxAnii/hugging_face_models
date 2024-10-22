905030c7198206b91e8153dea1ce65340608ce9b
MaxAnii <ansarbaba2000@gmail.com>
2024-10-22 19:49:18 +0530

    chore: updates setting.

--- hugging_face_models/settings.py
+++ hugging_face_models/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
+import os
 # Load environment variables from the .env file
 load_dotenv()
 
@@ -128,3 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
